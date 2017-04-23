e120625595f9794e7d6d3aae003fce9266f87b7e
rogrwhitakr <bennoosterholt@gmail.com>
2017-04-23 21:38:45 +0200

    start-process, windowed. no new window does not work

--- Powershell/Use-7ZipCompresson.ps1
+++ Powershell/Use-7ZipCompresson.ps1
@@ -23,6 +23,40 @@
     Author:         rogrwhikar
     Creation Date:  2017-04-21
     Purpose:        used in a script to be run daily 
+
+    7Zip:
+
+    <Commands>
+          a: Add files to archive
+          b: Benchmark
+          d: Delete files from archive
+          e: Extract files from archive (without using directory names)
+          l: List contents of archive
+          t: Test integrity of archive
+          u: Update files to archive
+          x: eXtract files with full paths
+    <Switches>
+          -ai[r[-|0]]{@listfile|!wildcard}: Include archives
+          -ax[r[-|0]]{@listfile|!wildcard}: eXclude archives
+          -bd: Disable percentage indicator
+          -i[r[-|0]]{@listfile|!wildcard}: Include filenames
+          -m{Parameters}: set compression Method
+          -o{Directory}: set Output directory
+          -p{Password}: set Password
+          -r[-|0]: Recurse subdirectories
+          -scs{UTF-8 | WIN | DOS}: set charset for list files
+          -sfx[{name}]: Create SFX archive
+          -si[{name}]: read data from stdin
+          -slt: show technical information for l (List) command
+          -so: write data to stdout
+          -ssc[-]: set sensitive case mode
+          -ssw: compress shared files
+          -t{Type}: Set type of archive
+          -u[-][p#][q#][r#][x#][y#][z#][!newArchiveName]: Update options
+          -v{Size}[b|k|m|g]: Create volumes
+          -w[{path}]: assign Work directory. Empty path means a temporary directory
+          -x[r[-|0]]]{@listfile|!wildcard}: eXclude filenames
+          -y: assume Yes on all queries
   
 .EXAMPLE
 
@@ -78,20 +112,24 @@ Function Use-7ZipCompression {
         switch ($Method) 
             { 
                 Add {
-                    & $tool a -mx9 -scsUTF-8 $archive.ToString() $Path | Tee-Object -Variable
+                    Start-Process -FilePath $tool -ArgumentList 'a', '-mx9', '-scsUTF-8', $archive.ToString(), $Path
                 } 
+                
                 Delete {
-                    & $tool d -mx9 -scsUTF-8 $archive $Path 
+                    Start-Process -FilePath $tool -ArgumentList 'd', '-mx9', '-scsUTF-8', $archive, $Path
                 } 
+                
                 Extract {
-                    #& $tool x -mx9 -scsUTF-8 $archive -o"$Path" -y
-                    Start-Process -FilePath $tool -ArgumentList 'x', '-mx9','-scsUTF-8 ', $archive, '-o$Path', '-y'
+                    Start-Process -FilePath $tool -ArgumentList 'x', '-mx9','-scsUTF-8 ', $archive, '-o$Path', '-y' -WindowStyle minimized
                 } 
+                
                 Test {
-                    & $tool t -mx9 -scsUTF-8 $archive
+                    Start-Process -FilePath $tool -ArgumentList 't', '-mx9','-scsUTF-8 ', $archive
+
                 } 
                 Update {
-                    & $tool u -mx9 -scsUTF-8 $archive $Path 
+                    Start-Process -FilePath $tool -ArgumentList 'u', '-mx9', '-scsUTF-8', $archive, $Path
+
                 } 
                 default {
                     Write-Output "No method selected."
@@ -99,65 +137,3 @@ Function Use-7ZipCompression {
             }
     }
 }
-
-$a = Get-ChildItem -Path "C:\Users\HaroldFinch\Desktop\2017-04-21.7z"
-
-$tool = Join-Path -Path ${env:ProgramW6432} -ChildPath ( Get-ChildItem -Path ${env:ProgramW6432} -Name "7z.exe" -Recurse  )
-
-& $tool x -mx9 -scsUTF-8 "C:\Users\HaroldFinch\Desktop\2017-04-21.7z"  -o"C:\Users\HaroldFinch\Pictures\demo" -y
-
-& $tool x -mx9 -scsUTF-8 "C:\Users\HaroldFinch\Desktop\zipfile.zip" "C:\Users\HaroldFinch\Desktop\"
-& $tool a -mx9 -scsUTF-8 "C:\Users\HaroldFinch\Desktop\zipfile.7z" $file1
-& $tool e -mx9 -scsUTF-8 -o 
-
-$pinfo = New-Object System.Diagnostics.ProcessStartInfo
-$pinfo.FileName = '$tool x -mx9 -scsUTF-8 "C:\Users\HaroldFinch\Desktop\2017-04-21.7z"  -o"C:\Users\HaroldFinch\Pictures\demo" -y'
-$pinfo.RedirectStandardError = $true
-$pinfo.RedirectStandardOutput = $true
-$pinfo.UseShellExecute = $false
-$pinfo.Arguments = ""
-$p = New-Object System.Diagnostics.Process
-$p.StartInfo = $pinfo
-$p.Start() | Out-Null
-$p.WaitForExit()
-$output = $p.StandardOutput.ReadToEnd()
-$output += $p.StandardError.ReadToEnd()
-$output | Out-File $myLog -Append
-
-#====>
-# das hier geht !!!!!
-Start-Process -FilePath $tool -ArgumentList 'x', '-mx9','-scsUTF-8 ', '"C:\Users\HaroldFinch\Desktop\2017-04-21.7z"', '-o"C:\Users\HaroldFinch\Pictures\demo"', '-y'
-#====>
-
-
-<Commands>
-  a: Add files to archive
-  b: Benchmark
-  d: Delete files from archive
-  e: Extract files from archive (without using directory names)
-  l: List contents of archive
-  t: Test integrity of archive
-  u: Update files to archive
-  x: eXtract files with full paths
-<Switches>
-  -ai[r[-|0]]{@listfile|!wildcard}: Include archives
-  -ax[r[-|0]]{@listfile|!wildcard}: eXclude archives
-  -bd: Disable percentage indicator
-  -i[r[-|0]]{@listfile|!wildcard}: Include filenames
-  -m{Parameters}: set compression Method
-  -o{Directory}: set Output directory
-  -p{Password}: set Password
-  -r[-|0]: Recurse subdirectories
-  -scs{UTF-8 | WIN | DOS}: set charset for list files
-  -sfx[{name}]: Create SFX archive
-  -si[{name}]: read data from stdin
-  -slt: show technical information for l (List) command
-  -so: write data to stdout
-  -ssc[-]: set sensitive case mode
-  -ssw: compress shared files
-  -t{Type}: Set type of archive
-  -u[-][p#][q#][r#][x#][y#][z#][!newArchiveName]: Update options
-  -v{Size}[b|k|m|g]: Create volumes
-  -w[{path}]: assign Work directory. Empty path means a temporary directory
-  -x[r[-|0]]]{@listfile|!wildcard}: eXclude filenames
-  -y: assume Yes on all queries
