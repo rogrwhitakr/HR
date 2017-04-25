03b45f6968c1d6fb6314e86d4b574692336d68a7
rogrwhitakr <bennoosterholt@gmail.com>
2017-04-25 21:28:51 +0200

    modules need to be appended to path, and checked

--- Powershell/Manage-Profile.ps1
+++ Powershell/Manage-Profile.ps1
@@ -27,7 +27,23 @@ try {
 
     If ((Test-Path $profile) -eq $true) {
 
-        $comp = Compare-Object -ReferenceObject $(Get-Content $profile) -DifferenceObject $(Get-Content -Path $conf)        if ($comp.Count -ne 0) {            Write-Host "The Powershell profile configuration file differs from the actual profile.`nThere are"$comp.Count "changes:"            Compare-Object -ReferenceObject $(Get-Content $profile) -DifferenceObject $(Get-Content -Path $conf)            Read-Host "Overwrite Powershell profile with profile.conf settings? Press Enter"            Set-Content -Path $profile -Value $(Get-Content -Path $conf)            Write-Host -ForegroundColor Green "Profile contents inserted"        }        else {            Write-Host "no changes between the files`n"$conf "`n and `n"$profile "`n detected."        }
+        $comp = Compare-Object -ReferenceObject $(Get-Content $profile) -DifferenceObject $(Get-Content -Path $conf)
+
+        if ($comp.Count -ne 0) {
+
+            Write-Host "The Powershell profile configuration file differs from the actual profile.`nThere are"$comp.Count "changes:"
+            Compare-Object -ReferenceObject $(Get-Content $profile) -DifferenceObject $(Get-Content -Path $conf)
+            Read-Host "Overwrite Powershell profile with profile.conf settings? Press Enter"
+            Set-Content -Path $profile -Value $(Get-Content -Path $conf)
+
+            Write-Host -ForegroundColor Green "Profile contents inserted"
+
+        }
+        else {
+
+            Write-Host "no changes between the files`n"$conf "`n and `n"$profile "`n detected."
+
+        }
         
     Write-Host -ForegroundColor Green "`nProfile Creation complete `n"
     }
@@ -42,6 +58,30 @@ catch {
 }
 
 
-#Write-Host -ForegroundColor Cyan "current profiles"
+
+
+
+
+
+#Write-Host -ForegroundColor Cyan "current profiles"
 #$profile | Format-List -Force
 Write-Host "Get-Help about_profiles`n==> full explanation of these paths. `n    The AllUsersAllHosts-profile is great if you want to`n    make a common set of functions or modules available to all users`n`n"
+
+
+Clear-Host
+Write-Host -ForegroundColor Cyan "Powershell Module Creation and Fill`n"
+
+Write-Host $env:psmodulepath
+
+$read = Read-Host -Prompt 'Module? Yes / No'
+
+if ( $read.Length -ne 0 ) {
+    
+    $mod_path = ${env:USERPROFILE} + "\Documents\WindowsPowerShell\" + $read
+    $env:psmodulepath = $env:psmodulepath + ";" + $mod_path
+
+}
+
+Write-Host "Appended psmodulepath ?"
+
+Write-Host $env:psmodulepath
