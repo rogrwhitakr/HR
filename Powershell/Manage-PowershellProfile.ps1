c8ebe3309bfd6ec5db616f4761aa45921c08176e
rogrwhitakr <bennoosterholt@gmail.com>
2017-04-25 20:59:45 +0200

    trying to module it

--- Powershell/Manage-PowershellProfile.ps1
+++
@@ -1,54 +0,0 @@
- # set conf location
- 
- $conf = "C:\repos\HR\Powershell\conf\profile.conf"
- 
- If ((Test-Path $conf) -ne $true) {
-    
-    Write-Error "
-    Error:
-        you must provide the path to the correct configuration file:
-        the file $conf does not exist"
-    Exit 1
- 
- }
-
- Clear-Host
- Write-Host -ForegroundColor Cyan "Powershell Profile Creation and Fill
- "
-
-try {
-
-    if ((Test-Path $profile) -eq $false) {
-        New-Item -Path $profile -ItemType File -Force
-    } 
-
-    If ((Test-Path $profile) -eq $true) {
-
-        $a = Get-Content -Path $conf
-
-        Set-Content -Path $profile -Value $a
-
-    Write-Host -ForegroundColor Green "Profile Creation complete
-    "
-
-    }
-}
-
-catch {
-
-    $ErrorMessage = $_.Exception.Message
-    Write-Host "An Error occurred. Please assign the Path to the Executable 7zip."
-    Write-Host $ErrorMessage
-
-}
-
-Write-Host -ForegroundColor Cyan "current profiles"
-$profile | Format-List -Force
-Write-Host 
-"Get-Help about_profiles 
-
-==> full explanation of these paths. 
-    The AllUsersAllHosts-profile is great if you want to 
-    make a common set of functions or modules available to all users"
-
-$comp = Compare-Object -ReferenceObject $(Get-Content $profile) -DifferenceObject $(Get-Content -Path $conf)if ($comp.Count -ne 0) {    Write-Host "do copy action here, because there are " $comp.count "changes"    Read-Host "Overwrite Powershell profile with profile.conf setting? Press Enter"}else {    Write-Host "do the regular thing"}
---
+++ Powershell/Manage-Profile.ps1
@@ -0,0 +1,47 @@
+#----------------------------------------------------------[Declarations]----------------------------------------------------------
+
+$conf = "C:\repos\HR\Powershell\conf\profile.conf"
+
+#-----------------------------------------------------------[Execution]------------------------------------------------------------
+
+
+Clear-Host
+Write-Host -ForegroundColor Cyan "Powershell Profile Creation and Fill`n"
+
+
+ If ((Test-Path $conf) -ne $true) {
+    
+    Write-Error "
+    Error:
+        you must provide the path to the correct configuration file:
+        the file $conf does not exist"
+    Exit 1
+ 
+ }
+
+try {
+
+    if ((Test-Path $profile) -eq $false) {
+        New-Item -Path $profile -ItemType File -Force
+    } 
+
+    If ((Test-Path $profile) -eq $true) {
+
+        $comp = Compare-Object -ReferenceObject $(Get-Content $profile) -DifferenceObject $(Get-Content -Path $conf)        if ($comp.Count -ne 0) {            Write-Host "The Powershell profile configuration file differs from the actual profile.`nThere are"$comp.Count "changes:"            Compare-Object -ReferenceObject $(Get-Content $profile) -DifferenceObject $(Get-Content -Path $conf)            Read-Host "Overwrite Powershell profile with profile.conf settings? Press Enter"            Set-Content -Path $profile -Value $(Get-Content -Path $conf)            Write-Host -ForegroundColor Green "Profile contents inserted"        }        else {            Write-Host "no changes between the files`n"$conf "`n and `n"$profile "`n detected."        }
+        
+    Write-Host -ForegroundColor Green "`nProfile Creation complete `n"
+    }
+}
+
+catch {
+
+    $ErrorMessage = $_.Exception.Message
+    Write-Host "An Error occurred. "
+    Write-Host $ErrorMessage
+
+}
+
+
+#Write-Host -ForegroundColor Cyan "current profiles"
+#$profile | Format-List -Force
+Write-Host "Get-Help about_profiles`n==> full explanation of these paths. `n    The AllUsersAllHosts-profile is great if you want to`n    make a common set of functions or modules available to all users`n`n"
