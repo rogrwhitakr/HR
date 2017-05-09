23c6c831549e876e960f7e57891f3afb1d7a1247
rogrwhitakr <bennoosterholt@gmail.com>
2017-05-09 20:37:34 +0200

    that is a nice one

---
+++ Powershell/google.ps1
@@ -0,0 +1,31 @@
+﻿function g {
+
+	Begin {
+
+		$query='https://www.google.com/search?q='
+
+	}
+
+	Process
+	{
+		if ($args.Count -eq 0)
+		{
+            break
+		}
+
+		Write-Host $args.Count, "Arguments detected"
+		Write-Host "Parsing out Arguments: $args"
+		for ($i=0;$i -le $args.Count;$i++){
+			$args | % {"Arg $i `t $_ `t Length `t" + $_.Length, " characters"} | Out-Null
+        }
+		$args | % {$query = $query + "$_+"}
+	}
+
+	End
+
+	{
+		$url = $query.Substring(0,$query.Length-1)
+		Write-Host "Search URL: $url `nInvoking..."
+		start "$url"
+	}
+}
