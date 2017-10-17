0e086e11e35fb6b0ee15803579d97904c9a88bd9
rogrwhitakr <bennoosterholt@gmail.com>
2017-10-17 20:08:33 +0200

    no need to be on repo, really

---
+++ PS/Manage-VirtualMachines.ps1
@@ -0,0 +1,37 @@
+﻿# startup / shutdown script 
+# to manage virtal machines
+# using the Virualbox CMDLETs from 
+# https://github.com/jdhitsolutions/PSVirtualBox
+
+# todo
+# move these to a config file, maybe?
+
+$northernlights = @(
+'dns',
+'server',
+'DNS-Server',
+'Mail-Server',
+'ubuntu-server',
+'SMB-Server',
+'dnsmasq',
+'powershell-core',
+'Admin'
+)
+
+function VM-StartUp {
+
+    foreach ($light in $northernlights) {
+        Start-VBoxMachine -Name $light -Headless -Verbose
+        Start-Sleep -Verbose -Seconds 2
+    }
+    
+}
+
+function VM-Shutdown {
+
+    $vms = Get-VBoxMachine
+
+    foreach ($vm in $vms) {
+        Stop-VBoxMachine -Name $vm.ID -Verbose
+    }
+}
