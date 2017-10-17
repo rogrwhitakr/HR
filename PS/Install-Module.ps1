c621c8874323186cc6dfb103565ee99c539ea9c7
rogrwhitakr <bennoosterholt@gmail.com>
2017-10-17 20:08:12 +0200

    quick explainer

---
+++ PS/Install-Module.ps1
@@ -0,0 +1,11 @@
+﻿# place script in
+# $home\Documents\WindowsPowerShell\Modules\<Module Folder>\<Module Files>
+#
+# the other paths work also
+($env:PSModulePath).Replace(";","`n")
+
+#\---Modules
+#    +---Use-7ZipCompresson
+#        +---Use-7ZipCompresson.psm1
+
+Import-Module -Name Tooling -Verbose 
