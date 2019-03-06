fd99bbcd0020440d00d1eb75ae1b9e3fc33d8bf1
rogrwhitakr <bennoosterholt@gmail.com>
2019-03-06 17:18:39 +0100

    rough check, works in principle

--- PS/incomplete/Get-Report.ps1
+++ PS/incomplete/Get-Report.ps1
@@ -1,75 +1,20 @@
 ﻿
-### Provides a report on computers ###
 
-# variables
-
-$rname = "NorthernLights Status Report"
-$rurl = "northernlights-report.html"
-
-$computers = "Z:\MyScripts\PowerShell\computers.csv"
-#$css = Get-Content -Path "$reportlocation\report.css"
-#$reportlocation = "C:\inetpub\wwwroot"
-
-function Get-InstalledList {
-    param (
-       [string] $ComputerName = '*'
-        )
-        
-        $installedList = Get-EventLog -LogName System -ComputerName $ComputerName | Where-Object {$_.EventID -eq 7045} | 
-        Select-Object TimeGenerated,MachineName,Message
-
-        #$installedList | ConvertTo-Html -Title $reportname -Head "<style>$css</style>"  | 
-        #out-file $reportlocation\$reporturl
-}
-
+# works in principle, needs some cleanup
 function New-Report {
     param (
-      #  [Object] $SendtoReport = '*',
-        [String] $ReportName = '*',
-        [String] $ReportUrl = '*'
-        )
+        [string] $ComputerName = '*'
+    )
+    $reportlocation = "C:\inetpub\wwwroot"
+    $css = Get-Content -Path "$reportlocation\report.css"
+    $reportname = "NorthernLights Status Report"
+    $reporturl = "northernlights-report.html"
+        
+    $installedList = Get-EventLog -LogName System -ComputerName $ComputerName | Where-Object {$_.EventID -eq 7045} | Select-Object TimeGenerated, MachineName, Message
+    $installedList | ConvertTo-Html -Title $reportname -Head "<style>$css</style>"  |         out-file $reporturl
 
-        $reportlocation = "C:\inetpub\wwwroot"
-        $css = Get-Content -Path "$reportlocation\report.css"
+}
 
-        ConvertTo-Html -Title $ReportName -Head "<style>$css</style>"  | 
-        out-file $reportlocation\$ReportUrl
-        }
+New-Report -ComputerName "Env:COMPUTERNAME"
 
-Get-InstalledList -ComputerName "Server" #| Create-Report -ReportName $rname -ReportUrl $rurl
-
-### Provides a report on computers ###
-
-# variables
-
-$computer = "Server"
-
-$reportname = "NorthernLights Status Report"
-$reporturl = "northernlights-report.html"
-$reportlocation = "C:\inetpub\wwwroot"
-
-#exec
-
-$css = Get-Content -Path "$reportlocation\report.css"
-$report = Get-EventLog -LogName System -ComputerName $computer | Where-Object {$_.EventID -eq 7045} | 
-    Select-Object TimeGenerated,MachineName,Message
-$report | ConvertTo-Html -Title '$reportname' -Head "<style>$css</style>" | 
-    out-file $reportlocation\$reporturl
-
-
-
-# do some reporting via html-file
-$css = Get-Content -Path "$reportlocation\report.css"
-$reportlocation = "C:\inetpub\wwwroot"
-
-Get-EventLog -LogName System | Where-Object {$_.EventID -eq 7045} | 
-    Select-Object TimeGenerated,MachineName,Message | 
-    ConvertTo-Html -Title "DOMAIN Server Report" -Head "<style>$css</style>"   | 
-    out-file $reportlocation\indicators.html
-
-$computerpath = "Z:\MyScripts\PowerShell"
-$computers = "computers.csv"
-if (Test-Path $computerpath != "true") {
-    Write-Host "geht ned"
-    }
-#Get-Content -Path $computers
+Read-Host -Prompt "somesin"
