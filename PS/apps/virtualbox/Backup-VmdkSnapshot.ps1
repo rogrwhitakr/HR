2b51df9c21828626a96dca9202c081aabdfb6dd4
rogrwhitakr <bennoosterholt@gmail.com>
2020-02-04 22:18:36 +0100

    removed commented section. good loop though

--- PS/apps/virtualbox/Backup-VmdkSnapshot.ps1
+++ PS/apps/virtualbox/Backup-VmdkSnapshot.ps1
@@ -2,32 +2,6 @@
 $backup_base_folder = "C:\Tools\backups"
 $vm_dir = "D:\VM\NorthernLights"
 $vboxmanage = "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe"
-#foreach ( $virtual_machine in (Get-ChildItem -Path $vm_dir -Directory) ) {
-
-    # get the virtual HDD. the sorting needs not to happen really. or the selecting...
-  #  $virtual_HDD = Get-ChildItem -path $virtual_machine.FullName -Recurse | `
-  #      Where-Object {$_.Extension -eq ".vmdk" -or $_.Extension -eq ".vdi" -and $_.FullName -notmatch "Snapshots"} | `
-  #      Sort-Object -Property LastWriteTime | Select-Object -First 1
-  #  'virtual_HDD is {0}' -f $virtual_HDD
-
-    # get the newest / last snapshot sorted by write time
-  #  $snapshot = Get-ChildItem -path $virtual_machine.FullName -Recurse | `
-  #      Where-Object {$_.Extension -eq ".vmdk" -or $_.Extension -eq ".vdi" -and $_.FullName -match "Snapshots"} | `
-  #      Sort-Object -Property LastWriteTime | Select-Object -Last 1
-  #  'snapshot is {0}' -f $snapshot 
-
-    # copy the hdd if not there yet
-    # robocopy might be the better tool here!
-  #  Copy-Item -Path $virtual_HDD.FullName -Destination $backup_dir -WhatIf
-
-  #"C:\Program Files\Oracle\VirtualBox\VBoxManage.exe"
-  # this one worked
- # VBoxManage clonevm SQL --uuid="2d03746f-24ed-4f0c-a181-25357a55912f" --basefolder="C:\Tools\backups" --name="SQL-backup" --mode="machine"
-
-    # if snapshot list empty, dont go any further!
-#}
-
-#Start-Process -FilePath $vboxmanage -ArgumentList 'list', '--sorted', 'vms' -NoNewWindow -PassThru
 
 $vms =  Get-VBoxMachine -All
 
