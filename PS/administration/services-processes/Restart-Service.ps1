e0d37f11e5543f1c4eea8d1d54d5a1e8f7dd5256
rogrwhitakr <bennoosterholt@gmail.com>
2020-05-14 11:03:53 +0200

    ok

---
+++ PS/administration/services-processes/Restart-Service.ps1
@@ -0,0 +1,22 @@
+﻿
+$servicename = 'northern-lights thingy'
+
+
+$service = Get-Service -Name $servicename -ComputerName $env:COMPUTERNAME
+
+if ($service.Status -eq 'Running' ) {
+
+    Write-Host -ForegroundColor Yellow 'Restarting '$service.DisplayName
+
+    $ServicePID = (get-wmiobject win32_service | Where-Object { $_.name -eq $service.Name}).processID
+
+    Write-Host  $ServicePID
+
+    if ($ServicePID) {
+
+        Restart-Service -Name $service
+    
+    }
+
+}
+# Stop-Process $ServicePID -Force
