50d22485b5949b9c65d85908542aeea4046008ad
rogrwhitakr <bennoosterholt@gmail.com>
2020-05-14 11:17:31 +0200

    works, as a activ programme

--- PS/apps/virtualbox/Backup-VmdkSnapshot.ps1
+++ PS/apps/virtualbox/Backup-VmdkSnapshot.ps1
@@ -1,49 +1,51 @@
-
+filter New-LogTimeStamp { 
+    "[ "+ (Get-Date -Format 'yyyy-hh-dd HH:mm:ss') + " ] :: "+ $_
+}
 
 $backup_base_folder = "C:\Tools\backups"
 $vboxmanage = "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe"
 $vms = Get-VBoxMachine
-$stop_required = $false
 $date = (Get-Date -Format 'yyyy-MM-dd')
 
-Start-Transcript -Path ($backup_base_folder + 'vm_backup.log') -Append
-Write-Verbose "Starting Backup of VirtualBox VM hard disks"
+Start-Transcript -Path (Join-path -Path $backup_base_folder -ChildPath ( $date + 'vm_backup.log')) -Append
+"Starting Backup of VirtualBox VM hard disks" | New-LogTimeStamp
 
 foreach ($vm in $vms) {
 
+    $stop_required = $false
     $uuid = [guid]::NewGuid()
-    Write-Warning -Message ('VM {0} is {1}. new UUID is {2}' -f ($vm.Name, $vm.state, $uuid))
+    'VM {0} is {1}. creating new UUID for backup: {2}' -f ($vm.Name, $vm.state, $uuid) | New-LogTimeStamp
 
     if ($vm.State -eq "Running") {
-        Write-Warning -Message ('stopping running VM "{0}"' -f $vm.Name)
+        'stopping running VM "{0}"' -f $vm.Name | New-LogTimeStamp
         Stop-VBoxMachine -Name $vm.Name
         Start-Sleep -Seconds 300
         $stop_required = $true
     }
    
     $stringBuilder = New-Object System.Text.StringBuilder 
-    $stringBuilder.Append(" clonevm ") 
-    $stringBuilder.Append($vm.Name) 
-    $stringBuilder.Append(" --uuid ") 
-    $stringBuilder.Append($uuid) 
-    $stringBuilder.Append(" --basefolder ") 
-    $stringBuilder.Append($backup_base_folder) 
-    $stringBuilder.Append(" --name ") 
-    $stringBuilder.Append( $date + "-" + $vm.Name) 
-    $stringBuilder.Append(" --mode=`"machine`" ") 
+    [void] $stringBuilder.Append(" clonevm ") 
+    [void] $stringBuilder.Append($vm.Name) 
+    [void] $stringBuilder.Append(" --uuid ") 
+    [void] $stringBuilder.Append($uuid) 
+    [void] $stringBuilder.Append(" --basefolder ") 
+    [void] $stringBuilder.Append($backup_base_folder) 
+    [void] $stringBuilder.Append(" --name ") 
+    [void] $stringBuilder.Append( $date + "-" + $vm.Name) 
+    [void] $stringBuilder.Append(" --mode=`"machine`" ") 
 
 
-    Write-Warning -Message ('starting clone process')
-    Start-Process -FilePath $vboxmanage -ArgumentList $stringBuilder -NoNewWindow -Wait
-    Write-Warning -Message ('clone process complete')
+    "starting clone process" | New-LogTimeStamp
+    Start-Process -FilePath $vboxmanage -ArgumentList $stringBuilder -NoNewWindow -Wait -ErrorAction (Write-Output -InputObject ('clone process failed')) 
+    'clone process complete' | New-LogTimeStamp
 
     if ($stop_required = $true) {
 
-        Write-Warning -Message ('starting VM "{0}" again' -f $vm.Name)
+        ('starting VM "{0}" again' -f $vm.Name) | New-LogTimeStamp
         Start-VBoxMachine -Name $vm.Name -Headless
         $stop_required = $false
     }
 }
 
-Write-Verbose "Backup of VirtualBox VM hard disks complete"
+"Backup of VirtualBox VM hard disks complete" | New-LogTimeStamp
 Stop-Transcript
