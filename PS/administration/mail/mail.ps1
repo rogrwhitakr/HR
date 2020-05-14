6e8e6966df6c23ba9bad6d7f8834f53fde8f0ffb
rogrwhitakr <bennoosterholt@gmail.com>
2020-05-14 10:08:29 +0200

    mail stuff

--- PS/administration/mail/mail.ps1
+++ PS/administration/mail/mail.ps1
@@ -7,11 +7,11 @@ $date = (Get-Date -Format yyyy-MM-dd_HH:mm)
 #  Preparing the Email properties
 $SmtpClient = New-Object -TypeName system.net.mail.smtpClient
 $SmtpClient.port="25"
-$SmtpClient.host="10.194.251.20"
+$SmtpClient.host="10.10.10.2"
 
 $MailMessage = New-Object -TypeName system.net.mail.mailmessage
-$MailMessage.from = "northern-lights.ce.production@northern-lights.com"
-$MailMessage.To.Add("rogrwitakr@northern-lights.com")
+$MailMessage.from = "northern-lights.production@northern-lights.one"
+$MailMessage.To.Add("rogrwitakr@northern-lights.one")
 
 #  Encoding
 $EmailEncoding = 'ASCII'
