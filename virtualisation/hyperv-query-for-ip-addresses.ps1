7ad7838924bafd1b29ba227f608af7b5063ab2e5
Benno Osterholt <bosterholt@corp.optitool.de>
2020-09-23 14:29:42 +0200

    base commands

---
+++ virtualisation/hyperv-query-for-ip-addresses.ps1
@@ -0,0 +1,19 @@
+# we must be administrator
+
+# get local vms
+Get-VM
+
+# get the names of running vms
+(Get-VM | Where-Object {$_.State -eq "running"}).Name
+
+# get the ip adresses
+Get-VM | Where-Object {$_.State -eq "running"} | Select-Object -ExpandProperty Networkadapters | Select-Object IPAddresses
+
+Write-Host "-----------------------------------"
+
+# as vars
+[array]$IPs = Get-VM | Where-Object {$_.State -eq "running"} | Select-Object -ExpandProperty Networkadapters | Select-Object IPAddresses
+
+$IPs | Get-Member
+
+$IPs.Length
