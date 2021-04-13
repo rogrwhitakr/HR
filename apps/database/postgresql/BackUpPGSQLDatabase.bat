8c305d303f31e95688888310f1b28b1a7fe2f290
HankScorpio <bennoosterholt@gmail.com>
2021-04-13 10:07:31 +0200

    added a dayOfWeek differentiator

--- apps/database/postgresql/BackUpPGSQLDatabase.bat
+++ apps/database/postgresql/BackUpPGSQLDatabase.bat
@@ -17,7 +17,7 @@ setlocal ENABLEDELAYEDEXPANSION
 ::			* db_name = Name DB
 ::		
 ::  Example
-::          * .\Create-Database-Backup.bat northernlights.database D:\_databases\pgsql\
+::          * .\Create-Database-Backup.bat database D:\_databases\pgsql\
 ::
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
@@ -28,12 +28,12 @@ set zip_path=C:\Program Files\7-Zip\
 
 set dialect=postgresql
 set port=5432
-set user=northernlights
-set PGPASSWORD=northernlights
+set user=postgres
+set PGPASSWORD=postgres
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-set version=1.41294
+set version=1.3
 set version=%version: =%
 set title=%~n0
 title %title% - Version: %version%
@@ -48,9 +48,21 @@ set month=%date:~-7,2%
 set day=%date:~-10,2%
 set mydate=%year%-%month%-%day%
 
+:: set the dayofweek
+for /f %%g in ('wmic path win32_localtime get dayofweek^|findstr /v /r "^$"') do (
+set DOW=%%g)
+
+if %DOW%==0 set DOW=Sonntag
+if %DOW%==1 set DOW=Montag
+if %DOW%==2 set DOW=Dienstag
+if %DOW%==3 set DOW=Mittwoch
+if %DOW%==4 set DOW=Donnerstag
+if %DOW%==5 set DOW=Freitag
+if %DOW%==6 set DOW=Samstag
+
 set db_name=%1
 set backup_path=%2
-set db_backup=%db_name%
+set db_backup=%db_name%_%DOW%
 set backup_log=%backup_path%%db_name%_backup.log
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
@@ -61,10 +73,10 @@ set backup_log=%backup_path%%db_name%_backup.log
 
 if not exist "%backup_path%%db_backup%_backup.log" echo Logfile for script %title% - Version: %version% > %backup_log%
 echo. >> %backup_log%
-echo %date% - %time% :: Starting Backup of database %db_name%, using dialect %dialect% >> %backup_log%
+echo %date% - %time% :: Starting Backup of database %db_name%, using database system %dialect% >> %backup_log%
 
 cd /D %sql_path%
-pg_dump.exe --username=%user% --no-password --host=localhost --port=%port% --dbname=%db_name% --file=%backup_path%%db_backup%.sql 2>> %backup_log%
+pg_dump.exe --username=%user% --no-password --host=localhost --port=%port% --format=custom --dbname=%db_name% --blobs --no-owner --no-privileges --file=%backup_path%%db_backup%.sql 2>> %backup_log%
 if %errorlevel% neq 0 ( set status_sql=BackUp failed. pg_dump.exe encountered a problem.) else (set status_sql=SQL Backup OK.)
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
