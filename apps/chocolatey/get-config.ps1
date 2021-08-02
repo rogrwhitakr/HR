6bf3b44b44308970f7939a778a815f0f8b65ac9a
HankScorpio <bennoosterholt@gmail.com>
2021-08-02 14:22:01 +0200

    renamed

--- apps/chocolatey/get-config.ps1
+++
@@ -1,2 +0,0 @@
-# get the current in cache config
-choco list -lo -r -y | Foreach-Object { "   <package id=`"$($_.SubString(0, $_.IndexOf("|")))`" version=`"$($_.SubString($_.IndexOf("|") + 1))`" />" }
