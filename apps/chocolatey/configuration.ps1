51d5d9459163b931d89b2b5043e8567299251be2
HankScorpio <bennoosterholt@gmail.com>
2021-10-13 16:58:28 +0200

    updated the chocolatey stuff

--- apps/chocolatey/configuration.ps1
+++
@@ -1,5 +0,0 @@
-# get the current in cache config
-choco list -lo -r -y | Foreach-Object { "   <package id=`"$($_.SubString(0, $_.IndexOf("|")))`" version=`"$($_.SubString($_.IndexOf("|") + 1))`" />" }
-
-# use the configuration
-choco install {path-to-file.config} --yes
---
+++ apps/chocolatey/installation-and-configuration.ps1
@@ -0,0 +1,21 @@
+# install (from privileged prompt)
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# upgrade
+choco upgrade chocolatey
+
+# install package
+choco install package
+choco install pgadmin4
+
+# get the current in cache config
+choco list -lo -r -y | Foreach-Object { "   <package id=`"$($_.SubString(0, $_.IndexOf("|")))`" version=`"$($_.SubString($_.IndexOf("|") + 1))`" />" }
+
+# get the currently installed list without added firleyfans
+choco list -lo -r -y | Foreach-Object {$($_.SubString(0, $_.IndexOf("|")))}
+
+# use the configuration
+choco install {path-to-file.config} --yes
+
+# install from configuration file
+choco install packages.config
