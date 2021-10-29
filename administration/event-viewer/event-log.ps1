72691949d79fa08cb16461ab41127ed06c1c2b84
HankScorpio <bennoosterholt@gmail.com>
2021-10-29 11:10:54 +0200

    moved to be found more easily

--- administration/event-viewer/event-log.ps1
+++ administration/event-viewer/event-log.ps1
@@ -1,4 +1,10 @@
+# get the services? that write a query-able event 
 Get-EventLog -log Application | Select-Object source -unique
+
+# query that service
+Get-EventLog -LogName Application -Source service-name
+
+# query system stuff
 Get-EventLog -log system | Select-Object source -unique
 
 Get-EventLog -LogName System -InstanceId 44 -Source Microsoft-Windows-WindowsUpdateClient |
