6a03ded287b822ea3de9f311ac4bd179498c433f
HankScorpio <bennoosterholt@gmail.com>
2021-11-30 09:24:41 +0100

    works somewhat

--- files-folders-executables/file-watcher.ps1
+++ files-folders-executables/file-watcher.ps1
@@ -1,13 +1,61 @@
-watcher = New-Object System.IO.FileSystemWatcher
+#requires -version 5.1
+
+$watcher = New-Object System.IO.FileSystemWatcher
+
+$watcher.Path = 'C:\Tools\database'
 
 $watcher.IncludeSubdirectories = $true
-$watcher.Path = 'C:\Tools\database'
 $watcher.EnableRaisingEvents = $true
+$watcher.Filter = "*.csv"
 
-$action = {
-    $path = $event.SourceEventArgs.FullPath
-    $changetype = $event.SourceEventArgs.ChangeType
-    Write-Host "$path was $changetype at $(get-date)"
+
+$Action = {
+    $details = $event.SourceEventArgs
+    $Name = $details.Name
+    $FullPath = $details.FullPath
+    $OldFullPath = $details.OldFullPath
+    $OldName = $details.OldName
+    $ChangeType = $details.ChangeType
+    $Timestamp = $event.TimeGenerated
+
+    $text = "{0} was {1} at {2}" -f $FullPath, $ChangeType, $Timestamp
+    Write-Host -message $text
+
+    # Define change types
+    switch ($ChangeType) {
+        'Changed' { "CHANGE" }
+        'Created' {
+            $text = "File {0} was created. we can now move it" -f $Name
+            Write-Warning -Message $text
+            Move-Item -Path $FullPath -Destination "C:\tools\database\copies" -verbose
+        
+        }
+        'Deleted' {
+            Write-Host "Deletion Started" -ForegroundColor Gray
+            Write-Warning -Message 'Deletion complete'
+        }
+        'Renamed' {
+            $text = "File {0} was renamed to {1}" -f $OldName, $Name
+            Write-Host $text -ForegroundColor Yellow
+        }
+        default { 
+            Write-Host $_ -ForegroundColor Red -BackgroundColor White 
+        }
+    }
 }
 
-Register-ObjectEvent $watcher 'Created' -Action $action
+# Set event handlers
+$handlers = . {
+    Register-ObjectEvent -InputObject $watcher -EventName Changed -Action $Action -SourceIdentifier FSChange
+    Register-ObjectEvent -InputObject $watcher -EventName Created -Action $Action -SourceIdentifier FSCreate
+    Register-ObjectEvent -InputObject $watcher -EventName Deleted -Action $Action -SourceIdentifier FSDelete
+    Register-ObjectEvent -InputObject $watcher -EventName Renamed -Action $Action -SourceIdentifier FSRename
+}
+
+while ($true) { start-sleep 5 }
+
+#Register-ObjectEvent $filewatcher "Created" -Action $writeaction
+#Register-ObjectEvent $filewatcher "Changed" -Action $writeaction
+#Register-ObjectEvent $filewatcher "Deleted" -Action $writeaction
+#Register-ObjectEvent $filewatcher "Renamed" -Action $writeaction
+#while ($true) {sleep 5}
