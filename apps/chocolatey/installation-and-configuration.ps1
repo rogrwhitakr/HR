9eb2578b6da2a50c6f22ea88d89803d72dcc508f
HankScorpio <bennoosterholt@gmail.com>
2021-12-09 12:28:34 +0100

    meh

--- apps/chocolatey/installation-and-configuration.ps1
+++ apps/chocolatey/installation-and-configuration.ps1
@@ -1,5 +1,5 @@
 # install (from privileged prompt)
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # upgrade
 choco upgrade chocolatey
