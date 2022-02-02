4a4b754f880048bddda8abe3b06e1b5fe9623fa0
HankScorpio <bennoosterholt@gmail.com>
2022-02-02 15:41:47 +0100

    changes after scriptanalyser run

--- data/xml/Remove-InvalidXML.ps1
+++ data/xml/Remove-InvalidXML.ps1
@@ -1,12 +1,13 @@
 function Test-XMLFile {
 
+    [OutputType([Boolean])]
     [CmdletBinding()]
     param
     (
         [String]
         [Parameter( Mandatory = $true, ValueFromPipeline = $true, Position = 1, HelpMessage = "File/Directory with xml files to check")]
         [ValidateNotNullorEmpty()]
-        [ValidateScript( {Test-Path $_ -include *.xml })]
+        [ValidateScript( { Test-Path $_ -include *.xml })]
         $Path
     )
 
@@ -36,57 +37,58 @@ function Remove-InvalidXML {
         $Remove
     )
 
-    try {
+    process {
+        try {
 
-        [int] $valid = 0
-        [int] $invalid = 0
-        $filepath = Get-ChildItem -Path  $Path | Where-Object { $_.Extension -eq '.xml' }
+            [int] $valid = 0
+            [int] $invalid = 0
+            $filepath = Get-ChildItem -Path  $Path | Where-Object { $_.Extension -eq '.xml' }
 
-        foreach ($file in $filepath) {
+            foreach ($file in $filepath) {
 
-            Set-Location -Path $file.DirectoryName
-            $result = Test-XMLFile -Path $file.FullName -ErrorAction Continue
+                Set-Location -Path $file.DirectoryName
+                $result = Test-XMLFile -Path $file.FullName -ErrorAction Continue
 
-            if ($result -eq $false) {
+                if ($result -eq $false) {
 
-                if ($Remove -eq $true) {
+                    if ($Remove -eq $true) {
 
-                    Write-Output "Removing invalid file $file"
-                    Remove-Item -Path $file
-                    $invalid = $invalid + 1
+                        Write-Output "Removing invalid file $file"
+                        Remove-Item -Path $file
+                        $invalid = $invalid + 1
 
+                    }
+
+                    else {
+
+                        Write-Output "Moving file $file to backup directory"
+                        $date = Get-Date -Format 'yyyy-MM-dd'
+                        $folder = New-Item -ItemType Directory -Name $date -Force
+                        Move-Item -Path $file -Destination $folder
+                        $invalid = $invalid + 1
+
+                    }
                 }
 
                 else {
-
-                    Write-Output "Moving file $file to backup directory"
-                    $date = Get-Date -Format 'yyyy-MM-dd'
-                    $folder = New-Item -ItemType Directory -Name $date -Force
-                    Move-Item -Path $file -Destination $folder
-                    $invalid = $invalid + 1
-
+                    Write-Output "$file is valid"
+                    $valid = $valid + 1
                 }
+            }
+            if ($Remove -eq $true) {
+                Write-Output "there were $valid valid XML files, $invalid invalid XML files.`n$invalid XML files have been deleted."
             }
 
             else {
-                Write-Output "$file is valid"
-                $valid = $valid + 1
+                Write-Output "there were $valid valid XML files, $invalid invalid XML files.`n$invalid XML files have been moved to a backup folder."
             }
         }
-        if ($Remove -eq $true) {
-            Write-Output "there were $valid valid XML files, $invalid invalid XML files.`n$invalid XML files have been deleted."
-        }
 
-        else {
-            Write-Output "there were $valid valid XML files, $invalid invalid XML files.`n$invalid XML files have been moved to a backup folder."
+        catch {
+
+            Write-Error $_.Exception.Message
+            Write-Error $_.Exception.Details
+
         }
     }
-
-    catch {
-
-        Write-Error $_.Exception.Message
-        Write-Error $_.Exception.Details
-
-    }
-
 }
