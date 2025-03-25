868bb84642c8b4dc3a7c65091aa0c5ac1565bc13
HankScorpio <bennoosterholt@gmail.com>
2025-03-25 09:41:07 +0100

    oke

--- administration/reporting/Get-InstalledSoftware.ps1
+++ administration/reporting/Get-InstalledSoftware.ps1
@@ -1,24 +1,18 @@
 
-$name = 'PostgreSQL'
-
-$regkey = "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall"
-
-$InstalledSoftware = Get-ChildItem $regkey 
-foreach ($Software in $InstalledSoftware) {
-
-#    Write-Host -message $Software.GetValue('DisplayName')
-#    Write-Host -message $Software.GetValue('DisplayVersion')
-#    Write-Host -message $Software.GetValue('Publisher')
-    Where-Object { ($Software.GetValue('Publisher') -match $name) } |  Write-Host -message $Software.GetValue('Publisher')
-#    Write-Host -message $Software.GetValue('Name')
-
-    Where-Object { ($Software.GetValue('Publisher') -match $name) -or`
-        ($Software.GetValue('Name') -match $name) -or`
-        ($Software.GetValue('DisplayName') -match $name) } | Write-Host '>AYAYAYAYAYAY'
-}
-
 # new method, does take a while, not? restricted to win platform
 Get-CimInstance -Class Win32_Product | Where-Object { ($_.vendor -match $name) -or ($_.name -match $name) }
 
-# Check recently installed software list from the Event Log remotely
-#Get-WinEvent -ProviderName msiinstaller | Where-Object {$_.id -eq 1033} | Select-Object timecreated,message | Format-List *
+# get all installed via registry
+Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*, HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*
+
+# look for a specific software
+Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*, HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Where-Object {$_.DisplayName -like "*notepad*"}
+
+# more display sthingy
+$regkey = "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall"
+$InstalledSoftware = Get-ChildItem $regkey 
+
+foreach ($Software in $InstalledSoftware) {
+    Write-Host -message $Software.GetValue('DisplayName')
+    $Software
+}
