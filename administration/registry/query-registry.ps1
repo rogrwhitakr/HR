c7f1e51e2d4da04460cd1a195cc3cfdc23f0d4c0
HankScorpio <bennoosterholt@gmail.com>
2025-08-25 13:07:58 +0200

    somesuch

--- administration/registry/query-registry.ps1
+++ administration/registry/query-registry.ps1
@@ -1,8 +1,8 @@
 
 # get paths of specific software
-Get-ItemProperty -Path "HKLM:SOFTWARE" -Name "Path64",'path'
+Get-ItemProperty -Path "HKLM:SOFTWARE" -Name "Path64", 'path'
 
-Get-ItemProperty -Path "HKLM:SOFTWARE\7-Zip" -Name "Path64",'path'
+Get-ItemProperty -Path "HKLM:SOFTWARE\7-Zip" -Name "Path64", 'path'
 
 # get OS version ID
 Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" | Select-Object -ExpandProperty ReleaseId
@@ -12,9 +12,33 @@ get-itemproperty '..\data\csv\demo_data.csv' | format-list
 
 get-itemproperty -path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\'
 
-Get-childItem -Path "registry::HKLM\SOFTWARE" | select *
+Get-childItem -Path "registry::HKLM\SOFTWARE" | Select-Object *
 
-foreach ($name in ((Get-childItem -Path "registry::HKLM\SOFTWARE").Name)){
-    Get-ItemProperty -Path $name -Name Name,Path64,path
+foreach ($name in ((Get-childItem -Path "registry::HKLM\SOFTWARE").Name)) {
+    Get-ItemProperty -Path $name -Name Name, Path64, path
 }
 
+
+$searchTerm = "Zabbix"
+$paths = @(
+    "HKLM:\SOFTWARE",
+    "HKCU:\SOFTWARE"
+)
+
+foreach ($path in $paths) {
+    Get-ChildItem -Path $path -Recurse -ErrorAction SilentlyContinue | ForEach-Object {
+        try {
+            $key = Get-ItemProperty -Path $_.PSPath -ErrorAction SilentlyContinue
+            foreach ($property in $key.PSObject.Properties) {
+                if ($property.Value -match $searchTerm) {
+                    Write-Output "Match found in: $($_.PSPath)"
+                    Write-Output "Property: $($property.Name) = $($property.Value)"
+                    Write-Output "----------------------------------------"
+                }
+            }
+        }
+        catch {
+        }
+    }
+}
+                                                            
